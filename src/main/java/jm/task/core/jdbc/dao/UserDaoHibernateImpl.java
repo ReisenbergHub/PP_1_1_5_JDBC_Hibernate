49e71d35cd2adfc2bba9704ba6ab6e2f943bd658
Reisenberg <yakovlev.a.official@yandex.ru>
2022-11-08 23:10:19 +0500

    add .gitignore. made the variables private

---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -0,0 +1,115 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UserDaoHibernateImpl implements UserDao {
+    private SessionFactory sessionFactory = Util.getSessionFactory();
+
+    public UserDaoHibernateImpl() {
+
+    }
+
+    @Override
+    public void createUsersTable() {
+        Transaction transaction = null;
+        try (final Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS User (id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
+                    "name VARCHAR(45), last_name VARCHAR(45), age INT);").addEntity(User.class).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        System.out.println("Table created");
+    }
+
+    @Override
+    public void dropUsersTable() {
+        Transaction transaction = null;
+        try (final Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS User;").addEntity(User.class).executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        System.out.println("Table dropped");
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        User user = new User(name, lastName, age);
+        Transaction transaction = null;
+        try (final Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        System.out.println("User " + name + " added in database");
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        Transaction transaction = null;
+        try (final Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        System.out.println("User with id " + id + " deleted");
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> userList = new ArrayList<>();
+        Transaction transaction = null;
+        try (final Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            userList = session.createSQLQuery("SELECT * FROM User;").getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        System.out.println("Userlist created");
+        return userList;
+    }
+
+    @Override
+    public void cleanUsersTable() {
+        Transaction transaction = null;
+        try (final Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("TRUNCATE TABLE User;").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        System.out.println("Table clear");
+    }
+}
